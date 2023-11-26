6fc8d7d49dc08c1d706e544b33b3214b9ae0b3f9
minyongP <86906386+Minon98@users.noreply.github.com>
2023-11-27 00:02:11 +0900

    #5 - repository interface 작성

--- src/main/java/com/example/projectboard/repository/ArticleCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectboard.repository;
-
-import com.example.projectboard.domain.ArticleComent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComent, Long> {
-}
--- src/main/java/com/example/projectboard/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectboard.repository;
-
-import com.example.projectboard.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
