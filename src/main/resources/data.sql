3514e21d2a03411d314155d58c6b5601f6b9bb3d
minyongP <86906386+Minon98@users.noreply.github.com>
2023-11-26 23:59:55 +0900

    "#5 - 테스트 데이터 추가"

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO article (title, content, hashtag, created_at, created_by, modified_at, modified_by) VALUES ('tFHelAZXyj', 'YYpGsCCsAXxmZc DvJdYLKYuRpmnWXtgVORxaXYbirHlUppuhk', 'PxcJP', '2020-08-10', 'VDAfbzCh', '2020-09-12', 'BGcROspB');
-INSERT INTO article (title, content, hashtag, created_at, created_by, modified_at, modified_by) VALUES ('fAEjBASYww', 'vSIKdgJoyjFxXcn KuDiNwMxrGMVylaSVxRWWBioxFsDlQrGXg', 'PvYoj', '2022-09-18', 'mOujtdFb', '2022-06-30', 'NXWLXWqe');
-INSERT INTO article (title, content, hashtag, created_at, created_by, modified_at, modified_by) VALUES ('praNOsONYw', 'v UwrUayWaDXmpYpobeKXuarEclddUaHmBjmlDFopeunGaDDHB', 'AXhIr', '2020-03-07', 'KcqreHyg', '2022-05-11', 'VVoFprPp');
-INSERT INTO article (title, content, hashtag, created_at, created_by, modified_at, modified_by) VALUES ('efwIkoXThT', 'prTWCIToNKyOYSFvL GIJCKgvudXiRgHnglCvNFPOtFtJiqCyY', 'TarxU', '2020-07-18', 'qafQfrps', '2021-12-19', 'wFTnzhLh');
-INSERT INTO article (title, content, hashtag, created_at, created_by, modified_at, modified_by) VALUES ('pRIWCwjFup', 'sQapcVBxcNCyGPYdlZQKRAESiGaUXrTlLXiNNzbdjY MPTVrRt', 'fQFUM', '2021-04-10', 'WgnFwIgP', '2020-04-06', 'vbJrpoPI');
-INSERT INTO article_coment (article_id, content, created_at, created_by, modified_at, modified_by) VALUES (1, 'This is a great article!', NOW(), 'user', NOW(), 'user');
-INSERT INTO article_coment (article_id, content, created_at, created_by, modified_at, modified_by) VALUES (1, 'Very informative, thank you.', NOW(), 'user', NOW(), 'user');
-INSERT INTO article_coment (article_id, content, created_at, created_by, modified_at, modified_by) VALUES (1, 'Interesting perspective, enjoyed reading.', NOW(), 'user', NOW(), 'user');
