de43b5357773043c1e950ca22f874dec6a51438f
minyongP <86906386+Minon98@users.noreply.github.com>
2023-11-28 00:26:21 +0900

    #8 - AuditingFields 추출
    엔티티 클래스에 들어가는 요소이고, 도메인과 직접 연관이 없어 추출 가능하다.
    '@MappedSuperclass'를 이용하여 상속 방식으로 추출함
    그외 '@DateTimeFormat' 요소 추가 등

--- src/main/java/com/example/projectboard/domain/ArticleComent.java
+++ src/main/java/com/example/projectboard/domain/ArticleComent.java
@@ -20,7 +20,7 @@ import java.util.Objects;
 })
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class ArticleComent {
+public class ArticleComent extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,20 +28,9 @@ public class ArticleComent {
     private Article article;
     @Setter @Column(nullable = false, length = 500)
     private String content;
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy;
-    @LastModifiedBy
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
-    @LastModifiedBy @Column(nullable = false, length = 100)
-    private String modifiedBy;
+
 
     protected ArticleComent() {
-
     }
 
     private ArticleComent(Article article, String content) {
