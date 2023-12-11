ef96387037eeea1d24f4e85784ecd98c6cb5c588
minyongP <86906386+Minon98@users.noreply.github.com>
2023-12-11 19:06:07 +0900

    #25 - 게시글 페이지네이션 구현
    첫 글, 마지막 글은 버튼 비활성화 및 가짜 링크 처리해야 해서
    타임리프 문법이 복잡해짐
    
    마지막 글을 판단하기 위해 총 글 개수가 필요해졌고
    이를 위해 count 쿼리 사용, 서비스 로직 추가

--- src/main/java/com/example/projectboard/controller/ArticleController.java
+++ src/main/java/com/example/projectboard/controller/ArticleController.java
@@ -45,6 +45,7 @@ public class ArticleController {
         ArticleWithCommentsResponse article =  ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.getArticleCommentsResponse());
+        map.addAttribute("totalCount", articleService.getArticleCount());
 
         return "articles/detail";
     }
--- src/main/java/com/example/projectboard/service/ArticleService.java
+++ src/main/java/com/example/projectboard/service/ArticleService.java
@@ -79,4 +79,7 @@ public class ArticleService {
         articleRepository.deleteById(articleId);
     }
 
+    public Long getArticleCount() {
+        return articleRepository.count();
+    }
 }
