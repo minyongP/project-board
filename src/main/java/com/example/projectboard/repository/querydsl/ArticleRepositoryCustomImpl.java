e0baa1703d00136337f91019d6e0e92c3c41ccc6
minyongP <86906386+Minon98@users.noreply.github.com>
2023-12-13 20:07:04 +0900

    #33 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를 정리해서 보여주는 기능을 넣기 위해, 해시태그만 뽑아서 unique하게 보여주는 쿼리를 querydsl로 작성함
    그 외 서비스 코드, 컨트롤러, 관련 테스트 작성

---
+++ src/main/java/com/example/projectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,24 @@
+package com.example.projectboard.repository.querydsl;
+
+import com.example.projectboard.domain.Article;
+import com.example.projectboard.domain.QArticle;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom{
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtag() {
+        QArticle article = QArticle.article;
+
+        return from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull())
+                .fetch();
+    }
+}
