b96ea52e0c095e3cb02b848271c7db88e6436aa3
minyongP <86906386+Minon98@users.noreply.github.com>
2024-01-02 15:38:38 +0900

    댓글 request 버그 수정
    of 가 아닌 new로 작성했어야함 이전 댓글 컨트롤러 테스트로 찾아낸 버그

--- src/main/java/com/example/projectboard/dto/request/ArticleCommentRequest.java
+++ src/main/java/com/example/projectboard/dto/request/ArticleCommentRequest.java
@@ -17,7 +17,7 @@ public class ArticleCommentRequest {
     }
 
     public static ArticleCommentRequest of(Long articleId, Long parentCommentId, String content) {
-        return ArticleCommentRequest.of(articleId, parentCommentId, content);
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
     public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
